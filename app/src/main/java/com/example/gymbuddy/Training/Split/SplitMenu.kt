0b1673994d501534137ef805eb947f193dcc2e13
SavageSaszo <sasik1998@tlen.pl>
2023-05-11 01:09:02 +0200

    Adding Split button functionality

---
+++ app/src/main/java/com/example/gymbuddy/Training/Split/SplitMenu.kt
@@ -0,0 +1,42 @@
+package com.example.gymbuddy.Training.Split
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.gymbuddy.R
+
+class SplitMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_split_menu)
+
+        val bb = findViewById<Button>(R.id.backandbiceps)
+
+        bb.setOnClickListener {
+            val intent = Intent(this, BackAndBiceps::class.java)
+            startActivity(intent)
+        }
+
+        val bs = findViewById<Button>(R.id.bellyandshoulders)
+
+        bs.setOnClickListener {
+            val intent = Intent(this, BellyAndShoulders::class.java)
+            startActivity(intent)
+        }
+
+        val ct = findViewById<Button>(R.id.chestandtriceps)
+
+        ct.setOnClickListener {
+            val intent = Intent(this, ChestAndTriceps::class.java)
+            startActivity(intent)
+        }
+
+        val legs = findViewById<Button>(R.id.legs)
+
+        legs.setOnClickListener {
+            val intent = Intent(this, Legs2::class.java)
+            startActivity(intent)
+        }
+    }
+}
